a602e42c59b18251ad2c37a9c84edfb95598662a
neogeek <neogeek0101@gmail.com>
2019-11-05 22:59:14 -0500

    Added editorInstallersUpdate to exports.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const getUnityUrls = require('./lib/get-unity-urls');
-const parsers = require('./lib/parsers');
+const editorInstallersUpdate = require('../lib/editor-installers-update');
+const getUnityUrls = require('../lib/get-unity-urls');
+const parsers = require('../lib/parsers');
 
 module.exports = {
+    editorInstallersUpdate,
     getUnityUrls,
     parsers
 };
