c7e9606eb3d2c550f4db1d93ad443096ba8c179f
neogeek <neogeek0101@gmail.com>
2019-11-06 01:17:13 -0500

    Added utilities to export.

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+const checkCacheExpiry = require('./utils/check-cache-expiry');
 const editorInstallersUpdate = require('./lib/editor-installers-update');
+const fetchWithLocalCache = require('./utils/fetch-with-local-cache');
 const getUnityUrls = require('./lib/get-unity-urls');
 const parsers = require('./lib/parsers');
 
 module.exports = {
+    checkCacheExpiry,
     editorInstallersUpdate,
+    fetchWithLocalCache,
     getUnityUrls,
     parsers
 };
