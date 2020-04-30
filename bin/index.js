7c901ac106940ee3d467283a2a2feca39094e49c
Scott Doxey <neogeek0101@gmail.com>
2020-04-30 19:26:18 -0400

    Formatted files.

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 const os = require('os');
-const {readFileSync} = require('fs');
-const {join} = require('path');
+const { readFileSync } = require('fs');
+const { join } = require('path');
 
 const chalk = require('chalk');
 const meow = require('meow');
@@ -13,7 +13,7 @@ const pkg = require('../package.json');
 
 const updateEditorInstallers = require('../lib/update-editor-installers');
 const getUnityUrls = require('../lib/get-unity-urls');
-const {parseVersionFromString} = require('../lib/parsers');
+const { parseVersionFromString } = require('../lib/parsers');
 
 const cli = meow(
     `
@@ -22,45 +22,49 @@ const cli = meow(
 
       Options
       ${chalk.yellow('--file, -f')}       Search file for Unity version number.
-      ${chalk.yellow('--force, -r')}      Force update to local cache of editor versions.
-      ${chalk.yellow('--offline, -o')}    Prevent request to update local cache of editor versions.
+      ${chalk.yellow(
+          '--force, -r'
+      )}      Force update to local cache of editor versions.
+      ${chalk.yellow(
+          '--offline, -o'
+      )}    Prevent request to update local cache of editor versions.
       ${chalk.yellow('--help, -h')}       Display this help message.
       ${chalk.yellow('--version, -v')}    Display the current installed version.
   `,
     {
-        'flags': {
-            'file': {
-                'alias': 'f',
-                'type': 'string'
+        flags: {
+            file: {
+                alias: 'f',
+                type: 'string'
             },
-            'force': {
-                'alias': 'r',
-                'default': false,
-                'type': 'boolean'
+            force: {
+                alias: 'r',
+                default: false,
+                type: 'boolean'
             },
-            'help': {
-                'alias': 'h',
-                'default': false,
-                'type': 'boolean'
+            help: {
+                alias: 'h',
+                default: false,
+                type: 'boolean'
             },
-            'offline': {
-                'alias': 'o',
-                'default': false,
-                'type': 'boolean'
+            offline: {
+                alias: 'o',
+                default: false,
+                type: 'boolean'
             },
-            'version': {
-                'alias': 'v',
-                'default': false,
-                'type': 'boolean'
+            version: {
+                alias: 'v',
+                default: false,
+                type: 'boolean'
             }
         }
     }
 );
 
 const osKeyMap = {
-    'Darwin': 'mac',
-    'Linux': 'linux',
-    'Windows_NT': 'win64'
+    Darwin: 'mac',
+    Linux: 'linux',
+    Windows_NT: 'win64'
 };
 
 const EDITOR_INSTALLERS_FILE_PATH = join(
@@ -70,52 +74,32 @@ const EDITOR_INSTALLERS_FILE_PATH = join(
 
 const DEFAULT_CACHE_TTL = 3600000;
 
-updateNotifier({pkg}).notify();
+updateNotifier({ pkg }).notify();
 
 if (cli.flags.file) {
-
     try {
-
-        cli.input[0] = parseVersionFromString(readFileSync(
-            cli.flags.file,
-            'utf8'
-        ));
-
-    } catch ({message}) {
-
+        cli.input[0] = parseVersionFromString(
+            readFileSync(cli.flags.file, 'utf8')
+        );
+    } catch ({ message }) {
         process.stderr.write(`${chalk.red('Error:')} ${message}`);
 
         process.exit(1);
-
     }
-
 }
 
 if (cli.flags.offline) {
-
-    getUnityUrls(
-        cli.input[0],
-        EDITOR_INSTALLERS_FILE_PATH
-    ).then(urls =>
-        process.stdout.write(`${urls[osKeyMap[os.type()]]}`));
-
+    getUnityUrls(cli.input[0], EDITOR_INSTALLERS_FILE_PATH).then(urls =>
+        process.stdout.write(`${urls[osKeyMap[os.type()]]}`)
+    );
 } else {
-
     updateEditorInstallers(
         EDITOR_INSTALLERS_FILE_PATH,
-        cli.flags.force
-            ? 0
-            : DEFAULT_CACHE_TTL
+        cli.flags.force ? 0 : DEFAULT_CACHE_TTL
     )
-        .catch(({message}) => {
-
+        .catch(({ message }) => {
             process.stderr.write(`${chalk.red('Error:')} ${message}`);
-
         })
-        .then(() => getUnityUrls(
-            cli.input[0],
-            EDITOR_INSTALLERS_FILE_PATH
-        ))
+        .then(() => getUnityUrls(cli.input[0], EDITOR_INSTALLERS_FILE_PATH))
         .then(urls => process.stdout.write(`${urls[osKeyMap[os.type()]]}`));
-
 }
